278f603df84c313feee2d1338af17bf92fce055a
Cheshire-Katze <serj.older@gmail.com>
2020-11-12 18:19:36 +0300

    script percent block skills succ

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,3 +11,10 @@ close.addEventListener('click', () => { //отслеживаем все клик
     menu.classList.remove('active'); // удаляем из класса "menu" класс "active"
 });
 
+const percents = document.querySelectorAll('.skills__level-percent'), // объявили переменную и присвоили ей все указанные классы
+    indicators = document.querySelectorAll('.skills__level-indicator span'); // объявили переменную и присвоили ей все span'ы
+//console.log(indicators); // в консоле видно, что в переменную percents мы добавили 6 классов skills__level-percent
+
+percents.forEach((item, i) => { //forEach - метод перебора; item - аргумент. Каждый элемент, который перебирается;  i - счетчик (1, 2, 3 и т.д)=> - стрелочная функция; {} - тело функции
+    indicators[i].style.width = item.innerHTML; // indicators[i].style.width - присваиваем width число (содержимое элемента) взятое из элемента skills__level-percent с помощью innerHTML. innerHTML - позволяет "вытащить" из элемента его содержимое, в данном случае, число из skills__level-percent
+});
